f8d749d83b0ddfd376161c88da4845815a09966b
M.huzaifa <m.huzaifa.ikhlaq@gmail.com>
2025-07-24 03:53:39 -0700

    banner working

--- Scirpt.js
+++ Scirpt.js
@@ -3,10 +3,27 @@
 
 // custom cursor 
 let customCursor = document.querySelector('.custom-cursor')
+let innerCursor = document.querySelector('.inner-cursor')
 
 document.addEventListener('mousemove', function (e) {
     customCursor.style.transform = `translate(${e.clientX}px,${e.clientY}px)`
 })
+//  banner img custom cursor 
+
+let bannerImg = document.querySelectorAll('.banner-img')
+
+bannerImg.forEach(img => {
+
+    img.addEventListener('mouseenter', () => {
+        customCursor.classList.remove('shadow-[0_0_30px_5px_rgba(255,255,0,0.2)]', '-left-[4.5px]', 'bg-white')
+        customCursor.classList.add('-translate-x-1/2', 'border', 'border-white', 'h-11', 'w-32.5', 'blur-[1px]')
+    })
+    img.addEventListener('mouseleave', () => {
+        customCursor.classList.add('shadow-[0_0_30px_5px_rgba(255,255,0,0.2)]', '-left-[4.5px]', 'bg-white')
+        customCursor.classList.remove('-translate-x-1/2', 'border', 'border-white', 'h-11', 'w-32.5', 'blur-[1px]')
+    })
+
+});
 
 // nav meanu 
 let nav = document.querySelector('#nav');
@@ -42,16 +59,20 @@ window.addEventListener('resize', function () {
 
 // arow jump animation 
 
-let arowContainer = document.querySelector('.arow-container')
-let arowsvg = document.querySelector('.arow-svg')
+let arowContainer = document.querySelectorAll('.arow-container')
 
-arowContainer.addEventListener('mouseenter', () => {
-    arowsvg.classList.remove('upjump', 'downjump')
-    void arowsvg.offsetWidth;
-    arowsvg.classList.add('upjump')
-})
-arowContainer.addEventListener('mouseleave', () => {
-    arowsvg.classList.remove('upjump', 'downjump')
-    void arowsvg.offsetWidth;
-    arowsvg.classList.add('downjump')
-})
+arowContainer.forEach(container => {
+    let arowsvg = container.querySelector('.arow-svg')
+
+    container.addEventListener('mouseenter', () => {
+        arowsvg.classList.remove('upjump', 'downjump')
+        void arowsvg.offsetWidth;
+        arowsvg.classList.add('upjump')
+    })
+    container.addEventListener('mouseleave', () => {
+        arowsvg.classList.remove('upjump', 'downjump')
+        void arowsvg.offsetWidth;
+        arowsvg.classList.add('downjump')
+    })
+});
+
