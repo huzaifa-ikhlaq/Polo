6dfcfa073c6964ce0f435d37d6e8b9223fb62e36
M.huzaifa <m.huzaifa.ikhlaq@gmail.com>
2025-07-25 07:15:34 -0700

    working on aviable animation and 2 section all complet

--- Scirpt.js
+++ Scirpt.js
@@ -16,7 +16,7 @@ bannerImg.forEach(img => {
 
     img.addEventListener('mouseenter', () => {
         customCursor.classList.remove('shadow-[0_0_30px_5px_rgba(255,255,0,0.2)]', '-left-[4.5px]', 'bg-white')
-        customCursor.classList.add('-translate-x-1/2', 'border', 'border-white', 'h-11', 'w-32.5', 'blur-[1px]')
+        customCursor.classList.add('-translate-x-1/2', 'border', 'border-white', 'h-11', 'w-32.5')
     })
     img.addEventListener('mouseleave', () => {
         customCursor.classList.add('shadow-[0_0_30px_5px_rgba(255,255,0,0.2)]', '-left-[4.5px]', 'bg-white')
@@ -57,6 +57,92 @@ window.addEventListener('resize', function () {
     }
 });
 
+// shooting stars animation 
+
+// shooting star animation 1
+
+let star1 = document.querySelector('.shooting-star-1')
+
+function starAnimation1() {
+    star1.classList.add('shooting-star-1')
+
+    setTimeout(() => {
+        star1.classList.remove('shooting-star-1')
+        setTimeout(() => {
+            starAnimation1()
+        }, 7000)
+    }, 3000)
+}
+
+starAnimation1()
+
+
+// shooting star animation 2
+
+let star2 = document.querySelector('.shooting-star-2')
+
+function starAnimation2() {
+    star2.classList.add('shooting-star-2')
+
+    setTimeout(() => {
+        star2.classList.remove('shooting-star-2')
+        setTimeout(() => {
+            starAnimation2()
+        }, 4000)
+    }, 3500)
+}
+
+starAnimation2()
+
+// shooting star animation 3
+
+let star3 = document.querySelector('.shooting-star-3')
+
+function starAnimation3() {
+    star3.classList.add('shooting-star-3')
+
+    setTimeout(() => {
+        star3.classList.remove('shooting-star-3')
+        setTimeout(() => {
+            starAnimation3()
+        }, 7000)
+    }, 3500)
+}
+
+starAnimation3()
+
+// shooting star animation 4
+
+let star4 = document.querySelector('.shooting-star-4')
+
+function starAnimation4() {
+    star4.classList.add('shooting-star-4')
+    setTimeout(() => {
+        star4.classList.remove('shooting-star-4')
+        setTimeout(() => {
+            starAnimation4()
+        }, 11000)
+    }, 5000)
+}
+
+starAnimation4()
+
+// shooting star animation 5
+
+let star5 = document.querySelector('.shooting-star-5')
+
+function starAnimation5() {
+    star5.classList.remove('shooting-star-5');
+    void star5.offsetWidth;
+    star5.classList.add('shooting-star-5');
+    setTimeout(() => {
+        starAnimation5();
+    }, 11000);
+}
+
+starAnimation5();
+
+
 // arow jump animation 
 
 let arowContainer = document.querySelectorAll('.arow-container')
@@ -75,4 +161,8 @@ arowContainer.forEach(container => {
         arowsvg.classList.add('downjump')
     })
 });
+
+// available Animation 
+
+let 
 
